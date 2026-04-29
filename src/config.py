114d9cbb83e563c0809a90d03e1b0b011157f452
Ayodele Mujib Ayoola <ayodelemujibayoola@gmail.com>
2026-04-29 23:34:56 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,10 @@
+HIGH_MATCH_THRESHOLD = 0.11
+MEDIUM_MATCH_THRESHOLD = 0.05
 
+PRIORITY_KEYWORDS = [
+    "python", "sql", "machine learning", "data analysis", "data science",
+    "data scientist", "structured data", "unstructured data", "statistics",
+    "statistical", "regression", "classification", "clustering", "pandas",
+    "numpy", "scikit learn", "matplotlib", "seaborn", "tensorflow",
+    "preprocessing", "visualization", "dataset", "models"
+]
